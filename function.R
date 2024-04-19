596f069abd4b1911c22f93aa84bcf2b6ac8a05f0
justin-git01 <vdoo0002@student.monash.edu>
2024-04-19 15:07:29 +1000

    Handle residuals in original scale

--- function.R
+++ function.R
@@ -3,9 +3,9 @@ library(fpp3)
 
 #### Final
 forecast_fun <- function(train, sales, hvi, period, length, fc_range){
-dat <- data.frame(train = log(train),
-                  sales = log(sales),
-                  hvi = log(hvi),
+dat <- data.frame(train = train,
+                  sales = sales,
+                  hvi = hvi,
                   Date = seq(as.Date("2013-07-01"), by = period, length.out = length))
 
 
@@ -24,58 +24,20 @@ else {
     model(VAR(vars(train, sales, hvi)))
 }
 
-# Extract residuals
 fitted_values <- fitted(fit)
-dat <- mutate(dat, 
-              fitted_train = exp(fitted_values$train),
-              fitted_sales = exp(fitted_values$sales),
-              fitted_hvi = exp(fitted_values$hvi))
 
 # Compute residuals in original scale
 dat <- mutate(dat,
-              residual_train_original = exp(train) - fitted_train,
-              residual_sales_original = exp(sales) - fitted_sales,
-              residual_hvi_original = exp(hvi) - fitted_hvi)
+              residual_train = exp(train) - exp(fitted_values$train)) 
 
-# Forecasting
-forecast_res <- forecast(fit, h = fc_range)
-forecast_output <- exp(as.numeric(forecast_res$.mean[, "train"]))  # Convert back from log scale
+dat$residual_train[is.na(dat$residual_train)] <- mean(dat$residual_train, na.rm = TRUE)
 
-# Collecting forecast and residuals
-residuals_original <- select(dat, residual_train_original, residual_sales_original, residual_hvi_original)
-output <- list(forecast_output = forecast_output, residuals_original = residuals_original)
-return(output)
+forecast_res <- fit |>
+  fabletools::forecast(h = fc_range) 
+
+train_res <- dat$residual_train
+forecast_output <- as.numeric(forecast_res$.mean[, "train"])
 
 output <- list(forecast_output, train_res)
 return(output)
 }
-
-
-
-train <- log(data$k1[1:108, 1])
-sales <- log(data$k1[1:108, 7])
-hvi <- log(data$k1[1:108, 8])
-
-dat <- data.frame(train = train,
-                  sales = sales,
-                  hvi = hvi,
-                  Date = seq(as.Date("2013-07-01"), by = "month", length.out = 108))
-
-
-dat <- dat %>% 
-  mutate(Month = yearmonth(Date)) |>
-  dplyr::select(-Date)  |>
-  as_tsibble(index = Month) |>
-  relocate(Month)
-
-fit <- dat |>
-  model(VAR(vars(train, sales, hvi)))
-
-forecast_res <- fit |>
-  fabletools::forecast(h = 12) 
-
-residuals <- residuals(fit)
-
-train_res <- as.numeric(residuals$train)
-forecast_output <- exp(as.numeric(forecast_res$.mean[, "train"]))
-
