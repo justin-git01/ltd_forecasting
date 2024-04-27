d99fe81f0e6eb0f674d8eb8fb99f166ba1cff916
justin-git01 <vdoo0002@student.monash.edu>
2024-04-27 15:16:44 +1000

    Create function to extract test set

---
+++ test_extract_fun.R
@@ -0,0 +1,16 @@
+test_extract <- function(data){
+  # Get the last month available in ltd_filtered
+  last_month <- max(data$Month)
+  
+  # Calculate the range for the next 12 months
+  start_ym <- yearmonth(as.Date(last_month) + days(1))
+  end_ym <- yearmonth(as.Date(last_month) + months(12) + days(1) - days(1))
+  
+  # Filter using 'yearmonth' range
+  required_data <- ltd_unit_train %>%
+    filter(Month > start_ym & Month <= end_ym) %>%
+    select(Total, NonRes, Comm, Ind, Other, Res) %>%
+    relocate(Month)
+  
+  return(t(required_data[,-1]))
+}
