87586e0044ca165ad0b5d95290c6d0abf2cb439c
justin-git01 <vdoo0002@student.monash.edu>
2024-04-27 15:19:19 +1000

    Build cross-validation for ARIMA model

--- Cross-validation.R
+++ Cross-validation.R
@@ -4,14 +4,23 @@ library(readxl)
 library(fpp3)
 library(urca)
 
+source("function.R")
+source("vecm_function.R")
+source("adjust_series_fun.R")
+
 # Load ltd aggregate date
-ltd_agg <- read_excel("data/LTD data aggregate.xlsx") %>%
+ltd_agg <- read_excel("data/LTD_new.xlsx", sheet = 1) |>
+  rename(Date = ...1,
+         ltd = LTD,
+         sales = SALES,
+         hvi = HVI) |>
   dplyr::select(c(Date, ltd, sales, hvi))
 
 # Load ltd unit data and join with aggregate data
-ltd_unit <- read_excel("data/LTD unit records summary.xlsx") |>
-  dplyr::select(dd, ltd_total, ltd_nonres, ltd_comm, ltd_ind, ltd_other, ltd_res) |>
-  left_join(ltd_agg, by = c("dd" = "Date")) |>
+ltd_unit <- read_excel("data/LTD_new.xlsx", sheet = 2) |>
+  rename(Date = ...1) |>
+  dplyr::select(Date, ltd_total, ltd_nonres, ltd_comm, ltd_ind, ltd_other, ltd_res) |>
+  left_join(ltd_agg, by = c("Date")) |>
   dplyr::select(-ltd)
 
 # Rename ltd unit data
@@ -24,12 +33,13 @@ ltd_unit <- ltd_unit %>%
   as_tsibble(index = Month) %>%
   relocate(Month)
 
-# Filter first 10 years only
-ltd_unit <- ltd_unit[1:120,]
+# Train-test set 
+ltd_unit_train <- ltd_unit[1:117,]
+ltd_unit_test <- ltd_unit[-c(1:117),]
 
-# Perform cross-validation with 5 years initially
-ltd_cv <- ltd_unit |>
-  stretch_tsibble(.init = 60, .step = 1)
+# Perform cross-validation with 6 years initially
+ltd_cv <- ltd_unit_train |>
+  stretch_tsibble(.init = 72, .step = 1)
 
 # Number of folds
 folds <- length(unique(ltd_cv$.id))
@@ -43,6 +53,26 @@ for (i in 1:folds){
   # Filter to fold
   ltd_filtered <- ltd_cv %>% filter(.id == i) %>% dplyr::select(-.id)
   
+  # Process each fold
+
+    # Get the last month available in ltd_filtered
+    last_month <- max(ltd_filtered$Month)
+    
+    # Calculate the range for the next 12 months
+    start_date <- last_month %m+% months(1)
+    end_date <- last_month %m+% months(12)
+    
+    # Extract the corresponding rows from ltd_unit_train
+    required_data <- ltd_unit_train %>%
+      filter(Month >= start_date & Month <= end_date) %>%
+      select(Total, NonRes, Comm, Ind, Other, Res)
+    
+    # Check if the extracted data has 12 months, otherwise continue with NA for this fold
+    if(nrow(required_data) == 12) {
+      # Assign the data to the test_set array for this fold
+      test_set[,,as.character(i)] <- as.matrix(required_data)
+    }
+  
   # Pre-define set of data
   data <- NULL
   base_fc <- NULL
@@ -50,53 +80,179 @@ for (i in 1:folds){
   test_fc <- NULL
   
   # Monthly series
-  data$k1 <- ts(ltd_filtered[, -1], frequency = 12, start = c(13, 7))
+  data$k1 <- ts(ltd_filtered[, -1], frequency = 12)
   colnames(data$k1) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res", "Sales", "hvi")
   
   # BI-MONTHLY SERIES
-  data$k2 <- ts(apply(data$k1, 2,
-                      function(x) colSums(matrix(x, nrow = 2))),
-                frequency = 6, start = c(13, 7))
+  data$k2 <- adjust_series(data$k1, freq = 6)
   
   # QUARTERLY SERIES
-  data$k3 <- ts(apply(data$k1, 2,
-                      function(x) colSums(matrix(x, nrow = 3))),
-                frequency = 4, start = c(13, 7))
+  data$k3 <- adjust_series(data$k1, freq = 4)
   
   # FOUR-MONTHLY SERIES
-  data$k4 <- ts(apply(data$k1, 2,
-                      function(x) colSums(matrix(x, nrow = 4))),
-                frequency = 3, start = c(13, 7))
+  data$k4 <- adjust_series(data$k1, freq = 3)
   
   # SEMI-ANNUAL SERIES
-  data$k6 <- ts(apply(data$k1, 2,
-                      function(x) colSums(matrix(x, nrow = 6))),
-                frequency = 2, start = c(13, 7))
+  data$k6 <- adjust_series(data$k1, freq = 2)
   
   # ANNUAL SERIES
-  data$k12 <- ts(apply(data$k1, 2,
-                       function(x) colSums(matrix(x, nrow = 12))),
-                 frequency = 1, start = c(13, 7))
+  data$k12 <- adjust_series(data$k1, freq = 1)
 
   # MONTHLY FORECASTS
   base_fc$k1 <- matrix(NA, nrow = 12, ncol = ncol(data$k1)-2)
-  residuals_fc$k1 <- matrix(NA, nrow = nrow(data), ncol = ncol(data$k1)-2)
+  residuals_fc$k1 <- matrix(NA, nrow = nrow(data$k1), ncol = ncol(data$k1)-2)
   for (i in 1:6) {
-    train <- values$k1[1:168, i]
-    forecast_res <- auto.arima(train)
-    base_fc$k1[, i] <- forecast_res[[1]]
-    residuals_fc$k1[, i] <- forecast_res[[2]]
+    train <- data$k1[, i]
+    forecast_arima <- forecast(auto.arima(train), h = 12)
+    base$k1[, i] <- forecast_arima$mean
+    residuals$k1[, i] <- forecast_arima$residuals
   }
-  base_fc$k1 <- ts(base_fc$k1, frequency = 12, start = c(10, 1))
+  base_fc$k1 <- ts(base_fc$k1, frequency = 12)
   colnames(base_fc$k1) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
   residuals_fc$k1 <- ts(residuals_fc$k1, frequency = 12)
   colnames(residuals_fc$k1) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
-  test_fc$k1 <- data$k1[-c(1:108), c(1:6)]
+  #test_fc$k1 <- data$k1[-c(1:108), c(1:6)]
+  
+  # BI-MONTHLY FORECASTS
+  base_fc$k2 <- matrix(NA, nrow = 6, ncol = ncol(data$k2)-2)
+  residuals_fc$k2 <- matrix(NA, nrow = nrow(data$k2), ncol = ncol(data$k2)-2)
+  for (i in 1:6) {
+    train <- data$k2[, i]
+    forecast_arima <- forecast(auto.arima(train), h = 6)
+    base$k2[, i] <- forecast_arima$mean
+    residuals$k2[, i] <- forecast_arima$residuals
+  }
+  base_fc$k2 <- ts(base_fc$k2, frequency = 6)
+  colnames(base_fc$k2) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  residuals_fc$k2 <- ts(residuals_fc$k2, frequency = 6)
+  colnames(residuals_fc$k2) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  #test_fc$k2 <- data$k2[-c(1:54), c(1:6)]
+  
+  # Quarterly
+  base_fc$k3 <- matrix(NA, nrow = 4, ncol = ncol(data$k3)-2)
+  residuals_fc$k3 <- matrix(NA, nrow = nrow(data$k3), ncol = ncol(data$k3)-2)
+  for (i in 1:6) {
+    train <- data$k3[, i]
+    forecast_arima <- forecast(auto.arima(train), h = 4)
+    base$k3[, i] <- forecast_arima$mean
+    residuals$k3[, i] <- forecast_arima$residuals
+  }
+  base_fc$k3 <- ts(base_fc$k3, frequency = 4)
+  colnames(base_fc$k3) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  residuals_fc$k3 <- ts(residuals_fc$k3, frequency = 4)
+  colnames(residuals_fc$k3) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  #test_fc$k3 <- data$k3[-c(1:36), c(1:6) ]
+  
+  # Four-monthly
+  base_fc$k4 <- matrix(NA, nrow = 3, ncol = ncol(data$k4)-2)
+  residuals_fc$k4 <- matrix(NA, nrow = nrow(data$k4), ncol = ncol(data$k4)-2)
+  for (i in 1:6) {
+    train <- data$k4[, i]
+    forecast_arima <- forecast(auto.arima(train), h = 3)
+    base$k4[, i] <- forecast_arima$mean
+    residuals$k4[, i] <- forecast_arima$residuals
+  }
+  base_fc$k4 <- ts(base_fc$k4, frequency = 3)
+  colnames(base_fc$k4) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  residuals_fc$k4 <- ts(residuals_fc$k4, frequency = 3)
+  colnames(residuals_fc$k4) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  #test_fc$k4 <- data$k4[-c(1:27), c(1:6) ]
   
   
-  base_arima_forecast[,,i] <- matrix(rnorm(12*6, 12, 6))
-  test_set[, ,i] <- matrix(rnorm(12*6, 12,6))
-  reconciled_arima[, , i] <- matrix(rnorm(12*6,12,6))
+  # Semi-annual
+  base_fc$k6 <- matrix(NA, nrow = 2, ncol = ncol(data$k6)-2)
+  residuals_fc$k6 <- matrix(NA, nrow = nrow(data$k6), ncol = ncol(data$k6)-2)
+  for (i in 1:6) {
+    train <- data$k6[, i]
+    forecast_arima <- forecast(auto.arima(train), h = 2)
+    base$k6[, i] <- forecast_arima$mean
+    residuals$k6[, i] <- forecast_arima$residuals
+  }
+  base_fc$k6 <- ts(base_fc$k6, frequency = 2)
+  colnames(base_fc$k6) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  residuals_fc$k6 <- ts(residuals_fc$k6, frequency = 2)
+  colnames(residuals_fc$k6) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  #test_fc$k6 <- data$k6[-c(1:18), c(1:6)]
+  
+  
+  # Annual
+  base_fc$k12 <- matrix(NA, nrow = 1, ncol = ncol(data$k12)-2)
+  residuals_fc$k12 <- matrix(NA, nrow = nrow(data$k12), ncol = ncol(data$k12)-2)
+  for (i in 1:6) {
+    train <- data$k12[, i]
+    forecast_arima <- forecast(auto.arima(train), h = 1)
+    base$k12[, i] <- forecast_arima$mean
+    residuals$k12[, i] <- forecast_arima$residuals
+  }
+  base_fc$k12 <- ts(base_fc$k12, frequency = 1)
+  colnames(base_fc$k12) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  residuals_fc$k12 <- ts(residuals_fc$k12, frequency = 1)
+  colnames(residuals_fc$k12) <- c("Total", "NonRes", "Comm", "Ind", "Other", "Res")
+  #test_fc$k12 <- data$k12[-c(1:9), c(1:6)]
+  
+  base <- t(do.call(rbind, rev(base_fc)))
+  res <- t(do.call(rbind, rev(residuals_fc)))
+
+  kset <- c(12, 6, 4, 3, 2, 1)
+  h <- 1
+  colnames(base) <- paste("k", rep(kset, h * rev(kset)), "_h",
+                          do.call("c", as.list(sapply(
+                            rev(kset) * h,
+                            function(x) seq(1:x)))),
+                          sep = "")
+  h <- 9
+  colnames(res) <- paste("k", rep(kset, h * rev(kset)), "_h",
+                         do.call("c", as.list(sapply(
+                           rev(kset) * h,
+                           function(x) seq(1:x)))),
+                         sep = "")
+  
+  C <- matrix(c(rep(1,4),
+                rep(1,3), 0), byrow = TRUE, nrow = 2)
+  colnames(C) <- c("Comm", "Ind", "Other", "Res")
+  rownames(C) <- c("Total", "NonRes")
+  #obs <- data
+  FoReco_data <- list(base = base,
+                      #test = test,
+                      res = res,
+                      C = C,
+                      #obs = obs)
+  
+  # Cross-sectional (contemporaneous) matrix
+  cs_info <- hts_tools(C = C)
+  
+  ut <- cs_info$Ut
+  
+  # Temporal matrix
+  te_info <- thf_tools(m = 12)
+  
+  Zt <- te_info$Zt
+  
+  # Reconciliation
+  ## cross-temp
+  #oct_recf_t_struc <- octrec(FoReco_data$base, m = 12, C = FoReco_data$C,
+                             comb = "t_struc", res = FoReco_data$res, keep = "recf")
+  
+  oct_recf_struc <- octrec(FoReco_data$base, m = 12, C = FoReco_data$C,
+                           comb = "struc", res = FoReco_data$res, keep = "recf")
+  
+  #oct_recf_wlsv <- octrec(FoReco_data$base, m = 12, C = FoReco_data$C,
+                          comb = "wlsv", res = FoReco_data$res, keep = "recf")
+  
+  discrepancy <- function(x, tol = sqrt(.Machine$double.eps)) {
+    cs <- max(abs(cs_info$Ut %*% x))
+    te <- max(abs(te_info$Zt %*% t(x)))
+    cat("cs discrepancy:", ifelse(cs>tol, sprintf("%.8f", cs), 0),
+        "\nte discrepancy:",ifelse(te>tol, sprintf("%.8f", te), 0))
+  }
+  discrepancy(oct_recf_struc)
+  
+  
+  for (j in 1: nrow(base)){
+    base_arima_forecast[j,,i] <- base[j, -c(1:16)]
+    test_set[j, ,i] <- matrix(rnorm(12*6, 12,6))
+    reconciled_arima[j, , i] <- oct_recf_struc[j, -c(1:16)]
+  }
 }
 
 colMeans(base_arima_forecast-test_set)
