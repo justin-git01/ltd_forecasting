2de4b7cb368c13007747beffa6065a7dcd86aed7
justin-git01 <vdoo0002@student.monash.edu>
2024-05-08 10:49:03 +1000

    Plotting all reconciled forecasts proportion against vecm based forecast for comparison

--- analysis04_cross_validation/Plotting_RMSE.R
+++ analysis04_cross_validation/Plotting_RMSE.R
@@ -1,26 +1,53 @@
-# Load necessary library
 library(dplyr)
 library(plotly)
 library(ggplot2)
 
-# Create a data frame for plotting
+# Plotting RMSE of base forecast to choose the benchmark 
+## Create a data frame for plotting
 rmse_base_data <- data.frame(
   h_step = 1:12,
-  RMSE_ARIMA = RMSE_arima_h_base[1,],
-  RMSE_VECM = RMSE_vecm_h_base[1,]
+  RMSE_ARIMA_base_total = RMSE_arima_h_base[1,],
+  RMSE_VECM_base_total = RMSE_vecm_h_base[1,]
 )
 
-# Reshape for ggplot
+## Convert to long format
 rmse_data_long <- rmse_base_data %>%
   pivot_longer(cols = -h_step, names_to = "Method", values_to = "RMSE")
 
 
-# Create the plot
+## Create the plot
 ggplot_rmse <- ggplot(rmse_data_long, aes(x = h_step, y = RMSE, colour = Method)) +
-  geom_line(linewidth = 1.2) +
-  labs(x = "H-step Forecast", y = "RMSE Value", title = "Forecast RMSE by Method") +
+  geom_line(linewidth = 1.1) +
+  labs(x = "h-step Forecast", y = "RMSE Value", title = "Base Total forecast RMSE by Method") +
+  scale_x_continuous(breaks = 1:12) +
   theme_minimal()
 
-# Convert ggplot to plotly
+## Convert ggplot to plotly
 ggplotly(ggplot_rmse)
 
+
+# Plotting all reconciled forecasts proportion against vecm based forecast for comparison
+prop_arima_vs_vecm_base <- (RMSE_arima_h_rec / RMSE_vecm_h_base)[1,]
+
+# Create a data frame for plotting
+prop_data <- data.frame(
+  h_step = 1:12,
+  Structural = RMSE_vecm_struc_prop[1,],
+  TCS = RMSE_vecm_tcs_prop[1,],
+  ARIMA_vs_VECM_Base = prop_arima_vs_vecm_base
+)
+
+# Reshape for ggplot
+prop_data_long <- prop_data %>%
+  pivot_longer(cols = -h_step, names_to = "Method", values_to = "Proportion")
+
+# Create the plot
+ggplot_prop <- ggplot(prop_data_long, aes(x = h_step, y = Proportion, colour = Method)) +
+  geom_line(linewidth = 1.1) +
+  geom_hline(yintercept = 1) +
+  labs(x = "H-step Forecast", y = "Proportion (Reconciled/Base Forecast)", title = "Proportion of Reconciled Forecast to Base VECM Forecast") +
+  scale_x_continuous(breaks = 1:12) +
+  theme_minimal()
+
+# Display the plot
+ggplotly(ggplot_prop)
