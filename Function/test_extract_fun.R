6a4cd39d5aa1a213e01a11f41798696db4403e0a
justin-git01 <vdoo0002@student.monash.edu>
2024-06-16 14:24:40 +1000

    Fix error in test extarct function

--- Function/test_extract_fun.R
+++ Function/test_extract_fun.R
@@ -6,17 +6,21 @@ test_extract <- function(data){
   start_ym <- yearmonth(as.Date(last_month) + days(1))
   end_ym <- yearmonth(as.Date(last_month) + months(12) + days(1) - days(1))
   
+  # Create a complete sequence of months for the next 12 months
+  full_month_seq <- seq(as.Date(start_ym), as.Date(end_ym), by = "month")
+  full_month_seq <- yearmonth(full_month_seq)
+  full_month_seq <- as_tsibble(data.frame(Month = full_month_seq), index = Month)
+  
   # Filter using 'yearmonth' range
   required_data <- ltd_unit %>%
     filter(Month > start_ym & Month <= end_ym) %>%
-    select(Total, NonRes, Comm, Ind, Other, Res) %>%
-    relocate(Month)
+    select(Month, Total, NonRes, Comm, Ind, Other, Res)
   
-  # Check if there is any data available in the specified range
-  if(nrow(required_data) == 0) {
-    return(NA)
-  } else {
-    return(t(required_data[,-7]))  # Transpose the data excluding the 'Month' column
-  }
+  # Join with the full sequence to fill missing months with NA
+  complete_data <- full_month_seq %>%
+    left_join(required_data, by = "Month")
+  
+  # Return the transposed data excluding the 'Month' column
+  return(t(complete_data[-1,-1]))
 }
 
