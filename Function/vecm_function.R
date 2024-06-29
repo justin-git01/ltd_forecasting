6a1f25096063576a578292ba08222599849ab7e2
justin-git01 <vdoo0002@student.monash.edu>
2024-06-29 15:36:17 +1000

    Add argument for lending variable, remove lag variable, use pre-defined function to select optimal lag based on input lag range

--- Function/vecm_function.R
+++ Function/vecm_function.R
@@ -1,10 +1,17 @@
 ###VECM 
 
-vecm_forecast_fun <- function(train, sales, hvi, period, length, fc_range, lag){
-  dat <- data.frame(train = log(train),
-                    sales = log(sales),
-                    hvi = log(hvi),
-                    Date = seq(as.Date("2013-07-01"), by = period, length.out = length))
+vecm_forecast_fun <- function(train, sales, hvi, lending, period, length, fc_range, include_lending = TRUE){
+  if(include_lending){
+    dat <- data.frame(train = log(train),
+                      sales = log(sales),
+                      lending = log(lending),
+                      Date = seq(as.Date("2013-07-01"), by = period, length.out = length))
+  } else {
+    dat <- data.frame(train = log(train),
+                      sales = log(sales),
+                      hvi = log(hvi),
+                      Date = seq(as.Date("2013-07-01"), by = period, length.out = length))
+  }
   
   dat_tsibble <- dat %>%
     mutate(Month = yearmonth(Date)) %>%
@@ -14,8 +21,21 @@ vecm_forecast_fun <- function(train, sales, hvi, period, length, fc_range, lag){
   
   dat1 <- ts(dat_tsibble[,-1], frequency = fc_range)
   
+  lag_range <- list(
+    "month" = 2:12,
+    "2 months" = 2:5,
+    "quarter" = 2:4,
+    "4 months" = 2:4,
+    "6 months" = 2
+  )
+  
+  lag_range_for_period <- lag_range[[period]]
+  
+  # Use pre-defined function to select optimal lag
+  optimal_lag <- select_optimal_lag(dat1, lag_range_for_period, include_lending)
+  
   # Johansen cointegration test to determine the rank
-  coint_test <- ca.jo(dat1, spec = "longrun", K = lag)  
+  coint_test <- ca.jo(dat1, spec = "longrun", K = optimal_lag)  
   
   r <- sum(coint_test@teststat > coint_test@cval[, "5pct"])  # Example: Using r=1
   
@@ -32,7 +52,7 @@ vecm_forecast_fun <- function(train, sales, hvi, period, length, fc_range, lag){
     # Construct residuals
     fitted_values <- fitted(vecm_fit)
     
-    fitted_train <- c(rep(mean(fitted_values[,1], na.rm = T),lag), fitted_values[,1] )
+    fitted_train <- c(rep(mean(fitted_values[,1], na.rm = T),optimal_lag), fitted_values[,1] )
     
     dat_tsibble <- mutate(dat_tsibble,
                           residual_train = exp(train) - exp(fitted_train)) 
@@ -51,6 +71,6 @@ vecm_forecast_fun <- function(train, sales, hvi, period, length, fc_range, lag){
   }
   else {
     warning("There is no cointegration (r=0), VAR model fitted instead")
-    var_forecast_fun(train, sales, hvi, period, length, fc_range)
+    var_forecast_fun(train, sales, hvi, lending, period, length, fc_range, include_lending)
   }
 }
