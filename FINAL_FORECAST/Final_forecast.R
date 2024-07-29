b96e77cea7aeca62ad4da962c6ddc613b4e98382
justin-git01 <vdoo0002@student.monash.edu>
2024-07-29 23:26:14 +1000

    Add seasonally adjusted and seasonality separation

--- FINAL_FORECAST/Final_forecast.R
+++ FINAL_FORECAST/Final_forecast.R
@@ -40,6 +40,114 @@ ltd_unit <- ltd_unit %>%
   as_tsibble(index = Month) %>%
   relocate(Month)
 
+# Filter out lending as it does not require seasonally adjusted
+lend <- ltd_unit |>
+  select(lending) |>
+  relocate(Month)
+
+# Total
+## Decomp
+dcmp <- ltd_unit |> 
+  model(stl = STL(Total))
+
+## Filter out SA
+total_ltd_sa <- components(dcmp) |>
+  select(season_adjust) |>
+  relocate(Month) |>
+  rename(Total = season_adjust)
+
+## Filter out seasonality
+total_ltd_s <- components(dcmp) |>
+  select(season_year) |>
+  relocate(Month) |>
+  rename(Total = season_year)
+
+# Residential
+## Decomp
+dcmp <- ltd_unit |> 
+  model(stl = STL(Res))
+
+## Filter out SA
+res_ltd_sa <- components(dcmp) |>
+  select(season_adjust) |>
+  relocate(Month) |>
+  rename(Res = season_adjust)
+
+## Filter out seasonality
+res_ltd_s <- components(dcmp) |>
+  select(season_year) |>
+  relocate(Month) |>
+  rename(Res = season_year)
+
+# NonRes
+## Decomp
+dcmp <- ltd_unit |> 
+  model(stl = STL(NonRes))
+
+## Filter out SA
+nonres_ltd_sa <- components(dcmp) |>
+  select(season_adjust) |>
+  relocate(Month) |>
+  rename(NonRes = season_adjust)
+
+## Filter out seasonality
+nonres_ltd_s <- components(dcmp) |>
+  select(season_year) |>
+  relocate(Month) |>
+  rename(NonRes = season_year)
+
+# Comm
+## Decomp
+dcmp <- ltd_unit |> 
+  model(stl = STL(Comm))
+
+## Filter out SA
+comm_ltd_sa <- components(dcmp) |>
+  select(season_adjust) |>
+  relocate(Month) |>
+  rename(Comm = season_adjust)
+
+## Filter out seasonality
+comm_ltd_s <- components(dcmp) |>
+  select(season_year) |>
+  relocate(Month) |>
+  rename(Comm = season_year)
+
+# Ind
+## Decomp
+dcmp <- ltd_unit |> 
+  model(stl = STL(Ind))
+
+## Filter out SA
+ind_ltd_sa <- components(dcmp) |>
+  select(season_adjust) |>
+  relocate(Month) |>
+  rename(Ind = season_adjust)
+
+## Filter out seasonality
+ind_ltd_s <- components(dcmp) |>
+  select(season_year) |>
+  relocate(Month) |>
+  rename(Ind = season_year)
+
+# other
+## Decomp
+dcmp <- ltd_unit |> 
+  model(stl = STL(Other))
+
+## Filter out SA
+other_ltd_sa <- components(dcmp) |>
+  select(season_adjust) |>
+  relocate(Month) |>
+  rename(Other = season_adjust)
+
+## Filter out seasonality
+other_ltd_s <- components(dcmp) |>
+  select(season_year) |>
+  relocate(Month) |>
+  rename(Other = season_year)
+
+# Sales
 # Decompositions
 dcmp <- ltd_unit |>
   model(stl = STL(Sales))
@@ -47,11 +155,46 @@ dcmp <- ltd_unit |>
 # Filter out sales trend
 sales_trend <- components(dcmp) |>
   select(trend) |>
-  relocate(Month)
+  relocate(Month) |>
+  rename(sales_trend = trend)
 
-# Join sales trend to ltd_unit
-ltd_unit <- ltd_unit |>
-  left_join(sales_trend, by = c("Month")) 
+# Filter out SA sales
+sales_sa <- components(dcmp) |>
+  select(season_adjust) |>
+  relocate(Month) |>
+  rename(Sales = season_adjust)
+
+# HVI
+# Decompositions
+dcmp <- ltd_unit |>
+  model(stl = STL(hvi))
+
+# Filter out SA hvi
+hvi_sa <- components(dcmp) |>
+  select(season_adjust) |>
+  relocate(Month) |>
+  rename(hvi = season_adjust)
+
+
+# Seasonal adjusted data
+sa_data <- total_ltd_sa |>
+  left_join(nonres_ltd_sa, by = c("Month")) |>
+  left_join(comm_ltd_sa, by = "Month") |>
+  left_join(ind_ltd_sa, by = "Month") |>
+  left_join(other_ltd_sa, by = "Month") |>
+  left_join(res_ltd_sa, by = "Month") |>
+  left_join(sales_sa, by = "Month") |>
+  left_join(hvi_sa, by = "Month") |>
+  left_join(lend, by = "Month") |>
+  left_join(sales_trend, by = "Month")
+
+# Seasonal data
+s_data <- total_ltd_s |>
+  left_join(nonres_ltd_s, by = c("Month")) |>
+  left_join(comm_ltd_s, by = "Month") |>
+  left_join(ind_ltd_s, by = "Month") |>
+  left_join(other_ltd_s, by = "Month") |>
+  left_join(res_ltd_s, by = "Month") 
 
 # Perform cross-validation with 8 years initially
 ltd_cv <- ltd_unit |>
